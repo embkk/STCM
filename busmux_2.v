582e492113c29103918113f0d24277116833dce5
K K <gpflv@ya.ru>
2025-10-22 19:09:29 +0300

    .bdf to Verilog for sim

---
+++ busmux_2.v
@@ -0,0 +1,29 @@
+// Copyright (C) 2018  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details.
+
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
+// CREATED		"Wed Oct 22 16:08:01 2025"
+
+
+module busmux_2(sel,dataa,datab,result);
+input sel;
+input [1:0] dataa;
+input [1:0] datab;
+output [1:0] result;
+
+busmux	lpm_instance(.sel(sel),.dataa(dataa),.datab(datab),.result(result));
+	defparam	lpm_instance.width = 2;
+
+endmodule
