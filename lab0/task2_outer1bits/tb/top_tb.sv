343638b6f778cc0539367778e951fd60729f6413
K K <gpflv@ya.ru>
2025-11-06 17:23:38 +0300

    Task 2 completed

--- lab0/task2_outer1bits/tb/top_tb.sv
+++ lab0/task2_outer1bits/tb/top_tb.sv
@@ -1,34 +1,37 @@
 module top_tb;
 
 logic [3:0] data;
-logic         data_val;
-logic         result_val;
+logic       data_val;
+logic       result_val;
 logic [3:0] result_left;
 logic [3:0] result_right;
 
 logic [3:0] test_left;
 logic [3:0] test_right;
-bit            test_passed;
+bit         test_passed;
 
 outer1bits outer1bits_inst (
-  .data_val_i                ( data_val      ),
-  .data_i                      ( data            ),
-  .data_val_o               ( result_val    ),
-  .data_left_o               ( result_left   ),
-  .data_right_o             ( result_right )
+  .data_val_i     ( data_val     ),
+  .data_i         ( data         ),
+  .data_val_o     ( result_val   ),
+  .data_left_o    ( result_left  ),
+  .data_right_o   ( result_right )
 );
 
 initial
   begin
+
     data = 0;
     test_passed = 1;
-    data_val = 1;
-
+    data_val = 0;
+    
     do
       begin
 
         test_left = 0;
         test_right = 0;
+
+        #10;
 
         for(int i = 0; i<4; i++ )
           begin
@@ -40,16 +43,23 @@ initial
                test_left = 4'b1000 >> i;
 
           end
-          
-        if( test_left != result_left || test_right != result_right )
+        
+        if( data_val !== result_val )
+            $display( "Error. Val %b expected, but %b", data_val, result_val );
+
+        if( data_val == 1 && test_left !== result_left || test_right !== result_right )
           begin
-            $display("Error %b. %b == %b && %b == %b expected", data, test_left, result_left, test_right, result_right );
+            $display( "Error %b. Left %b expected, but %b. Right %b expected, but %b", data, test_left, result_left, test_right, result_right );
             test_passed = 0;
           end
 
-        data+=4'b0001;
-
-        #10;
+	if(data_val == 0)
+          data_val = 1;
+        else
+          begin
+            data_val = 0;
+            data+=4'b0001;
+          end
 
       end
     while (data != 4'b0000);
