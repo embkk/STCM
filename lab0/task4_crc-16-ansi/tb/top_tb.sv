3e569d6d9fe7b6b330ecd96ab2d381f7be2292c6
K K <gpflv@ya.ru>
2025-11-19 12:24:04 +0300

    Task complete

--- lab0/task4_crc-16-ansi/tb/top_tb.sv
+++ lab0/task4_crc-16-ansi/tb/top_tb.sv
@@ -1,4 +1,7 @@
-module top_tb #(int NUM_TESTS = 1, int TEST_LENGTH = 32);
+module top_tb #(
+  int NUM_TESTS = 3,
+  int TEST_LENGTH = 32
+);
 
 logic           data;
 bit             clk;
@@ -8,10 +11,11 @@ logic  [15:0]   crc2;
 
 bit             test_passed;
 int             test_num;
+bit             test_data[NUM_TESTS][TEST_LENGTH];
+
+logic           test_feedback;
 logic   [15:0]  test_crc_ref;
 logic   [15:0]  test_crc_next;
-logic   [15:0]  crc_ref_reversed;
-bit             test_data[NUM_TESTS][TEST_LENGTH];
 
 initial
   begin
@@ -23,19 +27,20 @@ initial
   begin
     test_passed = 1;
     test_num = 0;
-    for (int t = 0; t < NUM_TESTS; t++) begin
-      for (int i = 0; i < TEST_LENGTH; i++) begin
-        test_data[t][i] = $urandom() % 2;
+    for ( int t = 0; t < NUM_TESTS; t++ )
+      begin
+        for ( int i = 0; i < TEST_LENGTH; i++ )
+          begin
+            test_data[t][i] = $urandom() % 2;
+          end
       end
-    end
   end
 
 crc16 crc16_inst (
   .clk_i            ( clk        ),
   .rst_i            ( rst        ),
   .data_i           ( data       ),
-  .crc_o            ( crc        ),
-  .crc2_o           ( crc2       )
+  .crc_o            ( crc        )
 );
 
 initial
@@ -45,72 +50,59 @@ initial
     data <= 0;
     rst <= 1'b0;
 
-    foreach (test_data[i])
+    foreach ( test_data[i] )
       begin
-        test_num++;
-        $display("\n=== Test %0d ===", test_num);
+        $display( "\n=== Test %0d ===", i );
 
-        // Сброс CRC перед тестом
-        @(posedge clk);
+        @( posedge clk );
         rst <= 1'b1;
-        @(posedge clk);
+        @( posedge clk );
         rst <= 1'b0;
 
-        // Подача битов данных
-        for (int j = 0; j < TEST_LENGTH; j++) begin
-          @(posedge clk);
+        for ( int j = 0; j < TEST_LENGTH; j++ )
+        begin
+          @( posedge clk );
           data <= test_data[i][j];
+          test_num++;
         end
 
-        // Ожидание финального результата
-        @(posedge clk);
+        @( posedge clk);
 
-        
-        check_crc16(crc_ref_reversed, crc);
-        check_crc16(crc_ref_reversed, crc2);
-        check_crc16(test_crc_ref, crc);
-        check_crc16(test_crc_ref, crc2);
-
+        if ( test_crc_ref === crc )
+          begin
+            $display( "Test %0d Success: DUT = 0x%04h | REF = 0x%04h", i, crc, test_crc_ref );
+          end
+        else
+          begin
+            $display( "Test %0d Failed. DUT | REF\n%16b - DUT\n%16b - REF", i, crc, test_crc_ref );
+            test_passed = 0;
+          end
       end
 
     if ( test_passed )
-      $display( "Tests: %d/%d. Passed.", test_num, NUM_TESTS );
+      $display( "All Tests - Passed. %d/%d", test_num, NUM_TESTS*TEST_LENGTH );
     else
-      $display( "Tests: %d/%d. Fail: errors above", test_num, NUM_TESTS );
+      $display( "All Tests - Fail. %d/%d", test_num, NUM_TESTS* NUM_TESTS*TEST_LENGTH );
 
     $stop;
   end
 
-task automatic check_crc16(
-    input logic [15:0] dut,
-    input logic [15:0] ref_val
-);
-    if (dut === ref_val) begin
-        $display("Test %0d Success: DUT = 0x%04h | REF = 0x%04h", test_num, dut, ref_val);
-    end else begin
-        $display("Test %0d Failed. DUT | REF\n%16b - DUT\n%16b - REF", test_num, dut, ref_val);
-        test_passed = 0;
-    end
-endtask
+always @( posedge clk or posedge rst )
+  begin
+    if ( rst )
+      test_crc_ref <= 16'h0000;
+    else
+      test_crc_ref <= test_crc_next;
+  end
 
-always @(posedge clk or posedge rst)
-begin
-  if (rst)
-    test_crc_ref <= 16'h0000;
-  else
-    test_crc_ref <= test_crc_next;
-    crc_ref_reversed <= {<<{test_crc_ref}};
-end
+always_comb
+  begin 
+    test_feedback = test_crc_ref[15] ^ data;
+    test_crc_next = {test_crc_ref[14:0], 1'b0};
 
-always_comb begin
-  logic feedback;
-
-  feedback = test_crc_ref[15] ^ data;
-  test_crc_next = {test_crc_ref[14:0], 1'b0};
-
-  test_crc_next[0] ^= feedback;
-  test_crc_next[2] ^= feedback;
-  test_crc_next[15] ^= feedback;
-end
+    test_crc_next[0] ^= test_feedback;
+    test_crc_next[2] ^= test_feedback;
+    test_crc_next[15] ^= test_feedback;
+  end
   
 endmodule
