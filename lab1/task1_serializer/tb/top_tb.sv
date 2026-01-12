1755b117c6d00429d10de205524c23d787493790
K K <gpflv@ya.ru>
2026-01-12 20:16:33 +0300

    Quartus compilation refactoring

---
+++ lab1/task1_serializer/tb/top_tb.sv
@@ -0,0 +1,36 @@
+module top_tb;
+
+parameter CLK_PERIOD = 5;
+
+// init before simulation without any sim event
+logic clk = 1'b0;
+logic rst = 1'b0;
+
+initial
+  begin
+
+    // reset
+    repeat(2) @(posedge clk);
+    rst <= 1'b1;
+
+    // idle
+    repeat(2) @(posedge clk);
+    rst <= 1'b0;
+    
+    // wait for test end
+    wait(testbench_pkg::testbench_all_finished);
+
+    testbench_pkg::testbench_print_stats();
+    
+    $stop;
+  end
+
+always #CLK_PERIOD clk = ~clk;
+
+// -- Testbench modules --
+serializer_tb serializer_tb_inst (
+  .clk_i          ( clk ),
+  .rst_i          ( rst )
+);
+  
+endmodule
